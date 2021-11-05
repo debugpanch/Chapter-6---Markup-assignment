e38669c81be23f5b539273631bed69df5e255b21
francisco castillo <franciscocastillo982@gmail.com>
2021-11-05 01:01:15 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+double calculateRetail(double, double);
+
+int main()
+{
+	double wholesale = 0, markup = 0, retailprice = 0;
+
+	cout << "Enter the item's wholesale price: ";
+	cin >> wholesale;
+
+	cout << "Enter the item's markup percentage: ";
+	cin >> markup;
+
+	retailprice = calculateRetail(wholesale, markup);
+	cout << "The Retail Price for the item is: $" << retailprice;
+	return retailprice;
+
+	while (wholesale < 0 || markup < 0)
+	{
+		cout << "Please Input Positive Values: ";
+			return 0;
+	}
+	_getch();
+}
+
+double calculateRetail(double cost, double percentage)
+{
+	double price = 0;
+	price = ((cost / 100) * percentage) + cost;
+	return price;
+
+}
